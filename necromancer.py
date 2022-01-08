a276b184397ebb8316cb52f316bd47353b3ec86e
nil-cipher <mmhomew@gmail.com>
2022-01-07 22:26:43 -0500

    added reserve spell function

--- necromancer.py
+++ necromancer.py
@@ -1,7 +1,6 @@
-
-
 from typing import Dict, Tuple
 import math
+import json
 
 
 def maintaining(num_spells: Dict[int, int], num_z) -> Dict[int, int]:
@@ -20,7 +19,8 @@ def maintaining(num_spells: Dict[int, int], num_z) -> Dict[int, int]:
     return maintaining_spells
 
 
-def creating(maintaining_spells: Dict[int, int], num_spells: Dict[int, int]) -> Tuple[Dict[int, int], int]:
+def creating(maintaining_spells: Dict[int, int],
+             num_spells: Dict[int, int]) -> Tuple[Dict[int, int], int]:
     creating = {}
     total = 0
     for i in range(3, 10):
@@ -28,7 +28,7 @@ def creating(maintaining_spells: Dict[int, int], num_spells: Dict[int, int]) -> 
             c = num_spells[i] - maintaining_spells.get(i, 0)
             if not c == 0:
                 creating[i] = c
-                total += (i-2) * c
+                total += (i - 2) * c
     return (creating, total)
 
 
@@ -43,33 +43,33 @@ def print_spells(spells: Dict[int, int], desc: str) -> None:
 
 def main():
     lines = []
-    num_spells = {}
     new = 0
+    params: Dict[str, int] = json.load(open("params.json"))
     # spell slots
-    with open('spells.txt') as f:
-        lines = f.readlines()
-    for l in lines:
-        lvl, num = list(map(int, l.split(" ")))
+    num_spells: Dict[int, int] = dict(
+        [int(a), x]
+        for a, x in params.get("spells", {}).items())  #convert keys to ints
+    # reserved spells
+    reserved_spells: Dict[int, int] = dict([int(a), x] for a, x in params.get(
+        "reserve_spells", {}).items())  #convert keys to ints
+    is_subclass = params["subclass"]
+    limited_max = 0
+    #input checks
+    for lvl, num in num_spells.items():
         if lvl < 0 or lvl > 9:
             raise ValueError('Invalid spell level.')
         if num < 0:
             raise ValueError(f'Invalid spell slots for level {lvl} spell.')
-        num_spells[lvl] = num
-        new += (lvl-2) * num
+        new += (lvl - 2) * num
+        limited_max += (4 + 2 *
+                        (lvl - 3)) * (num - reserved_spells.get(lvl, 0))
 
-    existing = 0
+    existing = params.get("existing", 0)
     day = 1
-    # parameters
-    with open("params.txt") as f:
-        lines = f.readlines()
-    for l in lines:
-        p, num = l.split(" ")
-        if p.lower() == "existing":
-            existing = int(num)
 
     maintaining_spells = maintaining(num_spells, existing)
     creating_spells, new = creating(maintaining_spells, num_spells)
-    while creating_spells:
+    while creating_spells and existing < limited_max:
         print("\nDay ", day, "-------------------------")
         print(f"Existing zombies: {existing}, new zombies: {new}")
         print_spells(maintaining_spells, "Maintaining Spells")
@@ -78,8 +78,10 @@ def main():
         maintaining_spells = maintaining(num_spells, existing)
         creating_spells, new = creating(maintaining_spells, num_spells)
         day += 1
-    print("-------------------------")
+    if existing > limited_max: existing = limited_max
+    print("\n-------------------------")
     print("Max zombie number reached:", existing)
+    print_spells(reserved_spells, "Reserved Spells")
 
 
 if __name__ == "__main__":
